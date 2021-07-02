46f5802c691171d94e2f204d8fab9400bfaf96f4
Lazaro Estrada <lazaromer97@gmail.com>
2021-07-02 00:31:13 -0400

    Create About component.

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,9 +1,17 @@
+import React from "react";
+
+import Card from "react-bootstrap/Card";
+
 import './About.css';
 
 const About = () => (
-	<div>
-		<h1>About</h1>
-	</div>
+	<Card className="text-center">
+		<Card.Header>About</Card.Header>
+		<Card.Body>
+			<Card.Title>Created by Lázaro Estrada</Card.Title>
+		</Card.Body>
+		<Card.Footer className="text-muted">Copyright © 2021 ToDo List Free, All rights reserved.</Card.Footer>
+	</Card>
 );
 
 export default About;
