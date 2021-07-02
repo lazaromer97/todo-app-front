28dfb43e644bb1b6d2d0a6840744350b5281b390
Lazaro Estrada <lazaromer97@gmail.com>
2021-07-02 02:13:39 -0400

    Create Todo component

--- src/pages/todo/Todo.js
+++ src/pages/todo/Todo.js
@@ -1,9 +1,28 @@
+import React from 'react';
+
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+
 import './Todo.css';
 
 const Todo = () => (
-	<div>
-		<h1>Todo</h1>
-	</div>
+	<Card className="text-center">
+		<Card.Header>ToDo</Card.Header>
+			<Card.Body>
+				<Container>
+					<Row>
+						<Col>
+						</Col>
+						<Col>
+						</Col>
+					</Row>
+				</Container>
+			</Card.Body>
+		<Card.Footer className="text-muted">Copyright © 2021 ToDo List Free, All rights reserved.</Card.Footer>
+	</Card>
 );
 
 export default Todo;
