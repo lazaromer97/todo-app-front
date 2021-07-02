9b9e0522dd1602c49f13ee6fe48b337b2a4fdca1
Lazaro Estrada <lazaromer97@gmail.com>
2021-07-02 00:22:51 -0400

    Create Home component.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,11 +6,13 @@ import Nav from 'react-bootstrap/Nav';
 
 import { Link } from 'react-router-dom';
 
+import './Header.css';
+
 const Header = () => {
     return (
         <Navbar bg="primary" variant="dark">
             <Container>
-                <Navbar.Brand as={Link} to="/" >TodoApp</Navbar.Brand>
+                <Navbar.Brand as={Link} to="/" >TodoListFree</Navbar.Brand>
                 <Nav className="me-auto">
                     <Nav.Link as={Link} to="/" >Home</Nav.Link>
                     <Nav.Link as={Link} to="/todo" >Todo</Nav.Link>
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,37 @@
-import './Home.css';
+import React from "react";
+
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+import "./Home.css";
 
 const Home = () => (
-	<div>
-		<h1>Home</h1>
-	</div>
+	<Card className="text-center">
+		<Card.Body>
+			<Card.Title>
+				What is a ToDo List?
+			</Card.Title>
+			<Card.Text>
+				What is a ToDo List? The definition is a simple one. It’s a list of
+				tasks you need to complete, or things that you want to do. Most
+				typically, they’re organised in order of priority. Traditionally,
+				they’re written on a piece of paper or post it notes and acts as a
+				memory aid. As technology has evolved we have been able to create a todo
+				lists with excel spreadsheets, word documents, email lists, todo list
+				apps, microsoft to do and google to do list to name a few. You can use a
+				to do list in your home and personal life, or in the workplace. Having a
+				list of everything you need to do written down in one place means you
+				shouldn’t forget anything important. By prioritising the tasks in the
+				list you plan the order in which you’re going to do them and can quickly
+				see what needs your immediate attention and what tasks you can leave
+				until a little later.
+			</Card.Text>
+			<Button variant="primary" href='/todo'>Go</Button>
+		</Card.Body>
+		<Card.Footer className="text-muted">
+			Copyright © 2021 ToDo List Free, All rights reserved.
+		</Card.Footer>
+	</Card>
 );
 
 export default Home;
